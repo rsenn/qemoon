f21bb72f4cc5e60e6048565dcf8c0836f01d2efb
eric.bellard <eric.bellard@cced4b2b-5c24-0410-8ab5-89cdd5fbaec3>
2006-12-13 22:23:44 +0000

    Initial import.

---
+++ trunk/qemoon/src/test/src/org/bellard/eqemu/LicenceAppender.java
@@ -0,0 +1,70 @@
+/*
+ * ========================================================================
+ *
+ * qemoon - a gui frontend for the qemu emulator written in the java programming language with the eclipse rcp framework.
+ * Copyright (C) 2006 Eric Bellard.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * ========================================================================
+ */
+/*
+ * ========================================================================
+ *
+ * qemoon - a gui frontend for the qemu emulator written in the java programming language with the eclipse rcp framework.
+ * Copyright (C) 2006 Eric Bellard.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * ========================================================================
+ */
+package org.bellard.eqemu;
+
+import java.io.File;
+import java.util.Enumeration;
+
+import org.apache.tools.ant.types.FileSet;
+
+/**
+ * @author Eric Bellard - eric.bellard@gmail.com
+ *
+ */
+public class LicenceAppender {
+
+	public final static void main(String[] args) {
+	FileSet fileset = new FileSet();
+	fileset.setDir(new File("/home/eric/dev/workspace/qemu/eqemu-rcp/src/java/src"));
+	fileset.setIncludes("**/*.java");
+
+	Enumeration e = fileset.selectorElements();
+	
+	}
+	
+	
+	
+	
+}
