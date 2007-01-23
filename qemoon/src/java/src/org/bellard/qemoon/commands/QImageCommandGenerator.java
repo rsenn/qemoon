a07cb31e8dc8ca528702994032ade0c0a6e21ad5
eric.bellard <eric.bellard@cced4b2b-5c24-0410-8ab5-89cdd5fbaec3>
2007-01-23 01:16:34 +0000

    many updates: - new project with new image - new wizzard - qemu-img usage - ihm updates - too much to write :-)

---
+++ qemoon/src/java/src/org/bellard/qemoon/commands/QImageCommandGenerator.java
@@ -0,0 +1,188 @@
+/*
+ * ========================================================================
+ *
+ * qemoon - a gui frontend for the qemu emulator written in the java programming language with the eclipse rcp framework.
+ * Copyright (C) 2006 Eric Bellard.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * ========================================================================
+ */
+package org.bellard.qemoon.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bellard.qemoon.utils.ValidatorUtils;
+
+/**
+ * @author Eric Bellard - eric.bellard@gmail.com
+ * 
+ */
+public class QImageCommandGenerator {
+
+	/** command */
+	public final static int CREATE_COMMAND = 1;
+
+	public final static int COMMIT_COMMAND = 2;
+
+	public final static int CONVERT_COMMAND = 3;
+
+	public final static int INFO_COMMAND = 4;
+
+	/** format */
+	public final static int DEFAULT_FORMAT = 0;
+
+	public final static int RAW_FORMAT = 1;
+
+	public final static int QCOW_FORMAT = 2;
+
+	public final static int COW_FORMAT = 3;
+
+	public final static int VMDK_FORMAT = 4;
+
+	public final static int CLOOP_FORMAT = 5;
+
+	protected int command;
+
+	protected int size;
+
+	protected String filename;
+
+	protected String qemuimgpath;
+
+	protected List<String> options;
+
+	protected QImageCommandGenerator(String qemuimgpath, int command,
+			List<String> options) {
+		this.qemuimgpath = qemuimgpath;
+		this.command = command;
+		this.options = options;
+	}
+
+	public String[] buildCommandArgument() {
+		List<String> arguments = new ArrayList<String>();
+
+		StringBuffer scommand = new StringBuffer();
+		arguments.add(qemuimgpath);
+		switch (command) {
+		case CREATE_COMMAND:
+			arguments.add("create");
+			break;
+		case COMMIT_COMMAND:
+			arguments.add("commit");
+			break;
+		case CONVERT_COMMAND:
+			arguments.add("convert");
+			break;
+		case INFO_COMMAND:
+			arguments.add("info");
+			break;
+		default:
+		}
+
+		arguments.addAll(options);
+
+		return arguments.toArray(new String[] {});
+	}
+
+	public final static QImageCommandGenerator buildCreateCommand(
+			String qemuimgpath, int size, String filename, int format,
+			boolean encrypt, String baseimage) {
+		List<String> options = new ArrayList<String>();
+		/* `create [-e] [-b base_image] [-f fmt] filename [size]' */
+
+		if (encrypt) {
+			options.add("-e");
+		}
+		if (!ValidatorUtils.isEmptyOrNull(baseimage)) {
+			options.add("-b");
+			options.add(baseimage);
+		}
+
+		options.addAll(buildFormat(format));
+
+		options.add(filename);
+
+		if (size != 0) {
+			options.add("" + size + "M");
+		}
+
+		QImageCommandGenerator imageGenerator = new QImageCommandGenerator(
+				qemuimgpath, CREATE_COMMAND, options);
+		return imageGenerator;
+	}
+
+	protected final static List<String> buildFormat(int format) {
+		List<String> formats = new ArrayList<String>();
+		if (format == DEFAULT_FORMAT) {
+			return formats;
+
+		}
+		formats.add("-f");
+		switch (format) {
+		case RAW_FORMAT:
+			formats.add("raw");
+			break;
+		case QCOW_FORMAT:
+			formats.add("qcow");
+			break;
+		case COW_FORMAT:
+			formats.add("cow");
+			break;
+		case VMDK_FORMAT:
+			formats.add("vmdk");
+			break;
+		case CLOOP_FORMAT:
+			formats.add("cloop");
+			break;
+		}
+		return formats;
+	}
+
+	public final static QImageCommandGenerator buildCommitCommand(
+			String qemuimgpath, String filename, int format) {
+
+		/* `commit [-f fmt] filename' */
+
+		List<String> options = buildFormat(format);
+		options.add(filename);
+		QImageCommandGenerator imageGenerator = new QImageCommandGenerator(
+				qemuimgpath, COMMIT_COMMAND, options);
+		return imageGenerator;
+	}
+
+	public final static QImageCommandGenerator buildInfoCommand(
+			String qemuimgpath, String filename, int format) {
+
+		/* `info [-f fmt] filename' */
+		List<String> options = buildFormat(format);
+		options.add(filename);
+		QImageCommandGenerator imageGenerator = new QImageCommandGenerator(
+				qemuimgpath, INFO_COMMAND, options);
+		return imageGenerator;
+	}
+
+	public final static QImageCommandGenerator buildConvertCommand(
+			String qemuimgpath, String filename, int format) {
+
+		/* `convert [-c] [-e] [-f fmt] filename [-O output_fmt] output_filename' */
+
+		// TODO implement buildConvertCommand
+		return null;
+
+	}
+
+}
---
+++ qemoon/src/java/src/org/bellard/qemoon/wizards/ProjectConfigWizardPage.java
@@ -0,0 +1,159 @@
+/*
+ * ========================================================================
+ *
+ * qemoon - a gui frontend for the qemu emulator written in the java programming language with the eclipse rcp framework.
+ * Copyright (C) 2006 Eric Bellard.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * ========================================================================
+ */
+/**
+ * 
+ */
+package org.bellard.qemoon.wizards;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+import org.bellard.qemoon.Activator;
+import org.bellard.qemoon.utils.ValidatorUtils;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.dialogs.IDialogPage;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * @author France Telecom
+ * @Copyright France Telecom S.A. 2006
+ */
+public class ProjectConfigWizardPage extends WizardPage {
+
+	private ISelection selection;
+
+	private Text memoryText;
+
+	private Text diskSizeText;
+
+	/**
+	 * Constructor for SampleNewWizardPage.
+	 * 
+	 * @param pageName
+	 */
+	public ProjectConfigWizardPage(ISelection selection) {
+		super("wizardPage");
+		setTitle("New VM Configuration Project...");
+		setDescription("This wizard creates a new VM Configuration project.");
+		this.selection = selection;
+	}
+
+	/**
+	 * @see IDialogPage#createControl(Composite)
+	 */
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 3;
+		layout.verticalSpacing = 9;
+		Label label = new Label(container, SWT.NULL);
+		label.setText(Activator.getDefault().getMessages().getString(
+				"preferences.memory.title"));
+
+		memoryText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		memoryText.setLayoutData(gd);
+		memoryText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+		label = new Label(container, SWT.NULL);
+		label.setText("Mb");
+
+		label = new Label(container, SWT.NULL);
+		label.setText(Activator.getDefault().getMessages().getString(
+				"preferences.harddisk.title"));
+
+		diskSizeText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		diskSizeText.setLayoutData(gd);
+		diskSizeText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+		label = new Label(container, SWT.NULL);
+		label.setText("Mb");
+
+		memoryText.setFocus();
+		setPageComplete(false);
+		setControl(container);
+	}
+
+	/**
+	 * Ensures that both text fields are set.
+	 */
+
+	private void dialogChanged() {
+
+		String tmemory = memoryText.getText();
+		if (!ValidatorUtils.isEmptyOrNull(tmemory)) {
+			if (!ValidatorUtils.isInt(tmemory)) {
+				updateStatus(Activator.getDefault().getMessages().getString(
+						"preferences.memory.4multiple"));
+				return;
+			}
+			int lmemory = ValidatorUtils.formatInt(tmemory);
+			if (lmemory % 4 != 0) {
+				updateStatus(Activator.getDefault().getMessages().getString(
+						"preferences.memory.4multiple"));
+				return;
+			}
+		}
+
+		String tdisk = diskSizeText.getText();
+		if (!ValidatorUtils.isEmptyOrNull(tdisk)) {
+			if (!ValidatorUtils.isInt(tdisk)) {
+				updateStatus(Activator.getDefault().getMessages().getString(
+						"preferences.harddisk.integer"));
+				return;
+			}
+		}
+		updateStatus(null);
+	}
+
+	private void updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+	public String getMemoryText() {
+		return memoryText.getText();
+	}
+
+	public String getHardDiskSizeText() {
+		return diskSizeText.getText();
+	}
+
+}
