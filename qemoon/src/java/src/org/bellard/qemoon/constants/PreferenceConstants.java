5a696ffb0e2b3a3cb2c69ed22e50444c695efd3b
eric.bellard <eric.bellard@cced4b2b-5c24-0410-8ab5-89cdd5fbaec3>
2007-01-25 23:10:48 +0000

    Add previously qemu image as main hard drive. refactoring. new labels

--- qemoon/src/java/src/org/bellard/qemoon/constants/PreferenceConstants.java
+++ qemoon/src/java/src/org/bellard/qemoon/constants/PreferenceConstants.java
@@ -27,40 +27,61 @@ package org.bellard.qemoon.constants;
 
 /**
  * @author Eric Bellard - eric.bellard@gmail.com
- *
+ * 
  */
 public interface PreferenceConstants {
 
 	public static final String PREFERENCES_QEMU_PATH_VALUE = "preferences.qemu.path.value";
+
 	public static final String PREFERENCES_QEMUIMG_PATH_VALUE = "preferences.qemuimg.path.value";
+
 	public static final String PREFERENCES_KQEMU_PATH_VALUE = "preferences.kqemu.path.value";
+
 	public static final String PREFERENCES_MONITOR_PORT_VALUE = "preferences.monitor.port.value";
+
 	public static final String PREFERENCES_MONITOR_SOCKETSERVER_VALUE = "preferences.monitor.socketserver.value";
 
-	
 	public static final String PREFERENCES_KQEMU_PATH_LABEL = "preferences.kqemu.path.label";
+
 	public static final String PREFERENCES_QEMU_PATH_LABEL = "preferences.qemu.path.label";
+
 	public static final String PREFERENCES_QEMUIMG_PATH_LABEL = "preferences.qemuimg.path.label";
+
 	public static final String PREFERENCES_MONITOR_PORT_LABEL = "preferences.monitor.port.label";
+
 	public static final String PREFERENCES_MONITOR_SOCKETSERVER_LABEL = "preferences.monitor.socketserver.label";
-	
+
 	public static final String PREFERENCES_TITLE = "preferences.title";
+
 	public static final String PREFERENCES_DESCRIPTION = "preferences.description";
+
 	public static final String PREFERENCES_SECTION = "preferences.section";
+
 	public static final String PREFERENCES_HARDDISK_TITLE = "preferences.harddisk.title";
+
 	public static final String PREFERENCES_MEMORY_TITLE = "preferences.memory.title";
+
 	public static final String PREFERENCES_AUDIO_TITLE = "preferences.audio.title";
+
 	public static final String PREFERENCES_CDROM_TITLE = "preferences.cdrom.title";
+
 	public static final String PREFERENCES_FLOPPY_TITLE = "preferences.floppy.title";
+
+	public static final String PREFERENCES_KEYBOARD_TITLE = "preferences.keyboard.title";
+
 	/**
 	 * 
 	 */
 	public static final String PREFERENCES_IMAGE_TITLE = "preferences.image.title";
+
 	public static final String PREFERENCES_MEMORY_LABEL = "preferences.memory.label";
+
 	public static final String PREFERENCES_MEMORY_DESCRIPTION = "preferences.memory.description";
-	
+
 	public static final String PREFERENCES_IMAGE_LABEL = "preferences.image.label";
+
 	public static final String PREFERENCES_IMAGE_CHECKLABEL = "preferences.image.checklabel";
+
 	public static final String PREFERENCES_IMAGE_SNAPSHOT = "preferences.image.snapshot";
 
 }
